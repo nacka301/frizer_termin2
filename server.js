6dc4eac1b6f4204768cf7fdabdf8010750fea29c
David <vackadavid@gmail.com>
2025-07-10 18:08:14 +0200

    Fix bcrypt hash for admin password

--- server.js
+++ server.js
@@ -113,7 +113,7 @@ app.post('/api/admin-login', async (req, res) => {
   try {
     // In a real application, you would fetch this from a database
     const adminUsername = 'admin';
-    const adminPasswordHash = '$2b$10$Z3geB0GyQdNF2k6pU.3PDO./rTustA8q2KFo/vo9dAWPXN5eijjXOe'; // bcrypt hash for 'password123'
+    const adminPasswordHash = '$2b$10$cL0UGgVIQFNzjV43gn5IA.cedI.JaOKh5zhjlC.ELFTZqbw6Naboy'; // bcrypt hash for 'password123'
 
     if (username === adminUsername && await bcrypt.compare(password, adminPasswordHash)) {
       req.session.isAdmin = true;
